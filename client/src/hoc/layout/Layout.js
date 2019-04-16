cec087a38ab7ffe1b10c0b050d64f836a5877f79
Adrian Durran <adrian@intellidroid.eu>
2019-04-16 14:45:54 +0400

    chore: client
    Initial layout

---
+++ client/src/hoc/layout/Layout.js
@@ -0,0 +1,13 @@
+import React, { Component, Fragment } from 'react';
+
+class Layout extends Component {
+  render() {
+    return (
+      <Fragment>
+        <main>{this.props.children}</main>
+      </Fragment>
+    );
+  }
+}
+
+export default Layout;
